13af02562e1df102c1f25240fed7fe88afeb4886
shiaifeng <345861448@qq.com>
2017-05-04 20:48:51 +0800

    随机下载图片

--- 仿SDWebImage/仿SDWebImage/ViewController.m
+++ 仿SDWebImage/仿SDWebImage/ViewController.m
@@ -8,11 +8,20 @@
 
 #import "ViewController.h"
 #import "DownloadOperation.h"
+#import "APPModel.h"
+#import "YYModel.h"
+#import "AFNetworking.h"
 
 @interface ViewController ()
 
 //全局队列
 @property(nonatomic,strong) NSOperationQueue *queue;
+
+@property (weak, nonatomic) IBOutlet UIImageView *iconImageView;
+
+@property(nonatomic,strong) NSArray *appList;
+
+@property(nonatomic,strong) NSMutableDictionary *opCache;
 
 @end
 
@@ -21,21 +30,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //加载数据
+    [self loadData];
+    
     //实例化队列
     _queue = [NSOperationQueue new];
     
-    //向自定义操作传入图片
-    NSString *urlStr = @"http://paper.taizhou.com.cn/tzwb/res/1/2/2015-01/20/12/res03_attpic_brief.jpg";
+    //实例化操作缓存池
+    self.opCache = [NSMutableDictionary new];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    //获取随机数
+    int random = arc4random_uniform((u_int32_t)self.appList.count);
+    
+    //获取模型
+    APPModel *model = self.appList[random];
     
     //创建操作
-    DownloadOperation *op = [DownloadOperation downloadImageWithUrlStr:urlStr andFinishedBlock:^(UIImage *image) {
-        NSLog(@"%@ %@",image,[NSThread currentThread]);
+    DownloadOperation *op = [DownloadOperation downloadImageWithUrlStr:model.icon andFinishedBlock:^(UIImage *image) {
+        
+        [NSThread sleepForTimeInterval:0.5];
+        
+        self.iconImageView.image = image;
     }];
     
     //将操作添加到队列
     [self.queue addOperation:op];
 }
 
+- (void)loadData
+{
+    NSString *urlStr = @"https://raw.githubusercontent.com/safabc123a/shiaifeng_Exercise01/master/apps.json";
+    
+    [[AFHTTPSessionManager manager] GET:urlStr parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        //字典转模型
+        self.appList = [NSArray yy_modelArrayWithClass:[APPModel class] json:responseObject];
+        
+        NSLog(@"appList %@",self.appList);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"错误信息 = %@",error);
+    }];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
